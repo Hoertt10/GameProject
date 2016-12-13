422c0675b1f56c7023ffa35478caf9d598b6d5e2
Kang-Wei Wu <j212172002@gmail.com>
2016-12-13 21:11:45 +0800

    finaly commit unity

---
+++ unity/Assets/program/hores/DefalueHorse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefalueHorse : MonoBehaviour {
+    public static int HorseNum = 2;
+    public GameObject[] horse = new GameObject[HorseNum];
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("DelayHorse1");
+    }
+    IEnumerator DelayHorse1()
+    {
+        yield return new WaitForSeconds(2);
+
+        StartCoroutine("DelayHorse2");
+        //ShowQusStatus = false;
+
+    }
+    IEnumerator DelayHorse2()
+    {
+        yield return new WaitForSeconds(2);
+        //ShowQusStatus = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
