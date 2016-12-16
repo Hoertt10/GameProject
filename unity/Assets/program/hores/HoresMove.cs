d49058308fa342bb49638baf8c558016fc1f4b8c
Kang-Wei Wu <j212172002@gmail.com>
2016-12-16 16:42:58 +0800

    add different horses move TimerX

--- unity/Assets/program/hores/HoresMove.cs
+++ unity/Assets/program/hores/HoresMove.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class HoresMove : MonoBehaviour {
-    private float TimerX,TimerY;
+     [SerializeField]private float TimerX,TimerY;
     private Vector3 pos;
 	// Use this for initialization
 	void Start () {
-        TimerX = -3f;
         pos = GameObject.Find("background2").transform.position - NewVector3(0,2f,0);
 	}
 	static Vector3 NewVector3(float x ,float y ,float z)
@@ -16,8 +15,8 @@ public class HoresMove : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-        TimerX += 0.01f;
-        TimerY += 0.5f;
+        TimerX += 0.02f;
+        TimerY += 0.05f;
         Vector3 rPos = pos;
         rPos.x = rPos.x + Mathf.Cos(TimerX) * 10f;
         rPos.y = rPos.y - (Mathf.Sin(TimerY)) * 1.5f;
@@ -28,6 +27,7 @@ public class HoresMove : MonoBehaviour {
         else if (rPos.x <= GameObject.Find("background2").transform.position.x - 7)
         {
             transform.rotation=Quaternion.Euler(0f,180f,0f);
+            
             
         }
         //Debug.Log((int)(Mathf.Cos(TimerX) * 100));
