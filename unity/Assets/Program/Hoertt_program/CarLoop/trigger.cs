0d9fd26f44536732c3fd402f66172b59b7b54c28
Hoertt <j212172002@gmail.com>
2017-01-11 22:21:23 +0800

    fix double collision and animal music

--- unity/Assets/Program/Hoertt_program/CarLoop/trigger.cs
+++ unity/Assets/Program/Hoertt_program/CarLoop/trigger.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class trigger : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    static int AniNum = 0;
+    void Start () {
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        sound.playmusic("dog");
-        Debug.Log("pass");
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-     
+        AniNum++;
+        switch(AniNum%3)
+        {
+            case 0:
+                sound.playmusic("dog");
+                break;
+            case 1:
+                sound.playmusic("cat");
+                break;
+            case 2:
+                sound.playmusic("sheep");
+                break;
+        }
+        Debug.Log(AniNum);
     }
     
     // Update is called once per frame
